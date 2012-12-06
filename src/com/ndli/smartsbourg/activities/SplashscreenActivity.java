77f55f2ef71221df18ca4487992327dee0d00ca3
Arthur Becker <arthur@chlorophyll-vision.com>
2012-12-06 21:54:37 +0100

    It works

--- src/com/ndli/smartsbourg/activities/SplashscreenActivity.java
+++ src/com/ndli/smartsbourg/activities/SplashscreenActivity.java
@@ -1,7 +1,23 @@
 package com.ndli.smartsbourg.activities;
 
+import com.ndli.smartsbourg.R;
+import com.ndli.smartsbourg.fragments.SplashscreenFragment;
+
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 
 public class SplashscreenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_splashscreen);
+
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		ft.replace(R.id.container, SplashscreenFragment.newInstance());
+		ft.commit();
+	}
 
 }
--- src/com/ndli/smartsbourg/fragments/SplashscreenFragment.java
+++ src/com/ndli/smartsbourg/fragments/SplashscreenFragment.java
@@ -1,7 +1,29 @@
 package com.ndli.smartsbourg.fragments;
 
-import android.app.Fragment;
+import com.ndli.smartsbourg.R;
 
-public class SplashscreenFragment extends Fragment{
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashscreenFragment extends Fragment {
+
+	public static SplashscreenFragment newInstance() {
+		SplashscreenFragment fragment = new SplashscreenFragment();
+		return fragment;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_splashscreen, null);
+		return v;
+	}
 
 }
