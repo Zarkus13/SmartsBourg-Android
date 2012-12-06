39385b1149e2f6e20545469ac1a0d363e845a55a
Arthur Becker <arthur@chlorophyll-vision.com>
2012-12-07 00:40:27 +0100

    commit

--- src/com/ndli/smartsbourg/fragments/EventsFragment.java
+++ src/com/ndli/smartsbourg/fragments/EventsFragment.java
@@ -1,13 +1,17 @@
 package com.ndli.smartsbourg.fragments;
 
-import com.ndli.smartsbourg.R;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+
+import com.ndli.smartsbourg.R;
+import com.ndli.smartsbourg.utils.DataProvider;
+import com.ndli.smartsbourg.utils.ListViewItemAdapter;
 
 public class EventsFragment extends Fragment {
 
@@ -19,6 +23,15 @@ public class EventsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_events, null);
 		ListView list = (ListView) v.findViewById(R.id.events_list);
+		list.setAdapter(new ListViewItemAdapter(DataProvider.currentEvents));
+
+		list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+			}
+		});
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 
--- src/com/ndli/smartsbourg/fragments/SmBgMapFragment.java
+++ src/com/ndli/smartsbourg/fragments/SmBgMapFragment.java
@@ -1,12 +1,7 @@
 package com.ndli.smartsbourg.fragments;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.google.android.gms.maps.MapFragment;
-import com.ndli.smartsbourg.R;
+
 
 public class SmBgMapFragment extends MapFragment {
 
