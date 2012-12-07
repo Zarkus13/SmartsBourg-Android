a1203c883c1516e8811ac1311a9b91d288938149
Alexis <zarkus13@gmail.com>
2012-12-07 02:18:59 +0100

    Centered the map

--- src/com/ndli/smartsbourg/fragments/SmBgMapFragment.java
+++ src/com/ndli/smartsbourg/fragments/SmBgMapFragment.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.MapFragment;
-import com.ndli.smartsbourg.R;
+import com.google.android.gms.maps.model.LatLng;
 
 public class SmBgMapFragment extends MapFragment {
 
@@ -14,6 +16,22 @@ public class SmBgMapFragment extends MapFragment {
 		SmBgMapFragment fragment = new SmBgMapFragment();
 		return fragment;
 	}
-	
-	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
+		getMap().setMyLocationEnabled(true);
+		
+		LatLng pos = new LatLng(48.58308, 7.747997);
+		CameraUpdate update = CameraUpdateFactory.newLatLngZoom(pos, 18);
+		
+		getMap().moveCamera(update);
+	}
 }
